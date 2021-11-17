f107c207e3bd709efe610342c76439575ddec75a
alexey <alexeyweb60@gmail.com>
2021-11-17 21:10:02 +0200

    Initial Commit

---
+++ diabetanddieta5/diabetanddieta5App.swift
@@ -0,0 +1,17 @@
+//
+//  diabetanddieta5App.swift
+//  diabetanddieta5
+//
+//  Created by alexey on 17.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct diabetanddieta5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
